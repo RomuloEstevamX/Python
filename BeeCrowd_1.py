534d55bd051fc09dccf4fc9b928c4d9e88d96f96
RomuloEstevamX <romuloestevamxavier@gmail.com>
2024-03-19 22:50:51 -0300

    BeeCrowd_Excercises

---
+++ BeeCrowd_1.py
@@ -0,0 +1,28 @@
+# 1000
+#print('Hello World!')
+
+#1001
+#A = int(input())
+#B = int(input())
+#X = A+B
+#print(F'X = {X}')
+
+#1002
+#raio = float(input())
+#n = 3.14159
+#area = float(n*raio**2)
+#print(F'A={area:.4f}')
+
+#1003
+#A = int(input())
+#B = int(input())
+#soma =  A+B
+#print(F'SOMA = {soma}')
+
+#1004
+#A = int(input())
+#B = int(input())
+#PROD = A*B
+#print(F'PROD = {PROD}')
+
+#1005
