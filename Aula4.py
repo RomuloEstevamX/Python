58014242b6f76f62b9bc81fb6225e069b8f785d9
Rômulo EstevamX <romuloestevamxavier@gmail.com>
2024-04-08 20:03:14 -0300

    Aula 4

---
+++ Aula4.py
@@ -0,0 +1,13 @@
+# # x = 0
+# # while  x <100:
+# #     print(x+1)
+# #     x = x+1
+# # x = 50
+# # while x<100:
+# #     print(x)
+# #     x += 1
+# x = 10
+# while x>=0:
+#     print(x)
+#     x-= 1
+# print('FOGO!')
